9fbd9bb6e21b0a0d1b97893ff245efc1faa72023
chigozieokoroafor <okoroaforc14@gmail.com>
2022-12-05 05:05:35 +0100

    made changes to connection host

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from backend import app
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", debug=True, port=10000)
-    app.run(host="10.220.85.78", debug=True, port=10000)
+    app.run(host="0.0.0.0", debug=True, port=10000)
+    #app.run(host="10.220.85.78", debug=True, port=10000)
